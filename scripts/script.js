9062945c6ba9ab183765c1a8a2b33e3a4c7b1c83
SarahGieben <90245294+SarahGieben@users.noreply.github.com>
2021-10-05 13:59:58 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  deNav = event.target.parentNode;
+  deNav.classList.toggle("toonMenu");
+}
+
+
